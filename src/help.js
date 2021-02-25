bc7c57ebacfa6dd387987bf82201fa694cc5edb6
L-Mo <78338200+L-M0@users.noreply.github.com>
2021-02-25 12:28:05 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,214 @@
+const help = (prefix, uptime, jam, tanggal, positif, meninggal, sembuh, perawatan) => { 
+	return `
+
+❒「 *𝗚𝗙𝗟* 」
+│
+┝⋗  HOURS : *${jam} WIB*
+┝⋗  DATE : *${tanggal}*
+┝⋗  VERSION: *1.2*
+┝⋗   OWNER : 𝙇╸𝙈𝟬
+┢━━━━━❪㊕❫━━━━━
+┣━━‣ *𝘾𝙊𝙑𝙄𝘿*
+┃
+┃⍣ *positive* : ${positif}
+┃⍣ *died*  : ${meninggal}
+┃⍣ *recover* :  ${sembuh}
+┃⍣ *in care* : ${perawatan}
+┣━━━━━❪㋛❫━━━━━
+┣━━‣ *𝗺𝗮𝗶𝗻𝘁𝗲𝗻𝗮𝗻𝗰𝗲*
+┃
+┗━━━━⟮注意⟯━━━━━
+
+❒『 *GFL BOT* 』
+│
+┝⋗ *${prefix}info*
+┝⋗ *${prefix}donasi*
+┝⋗ *${prefix}owner*
+┝⋗ *${prefix}speed*
+┝⋗ *${prefix}enaknya* [request]
+┝⋗ *${prefix}report [lapor bug]*
+┕────⟮⚅⟯────
+
+*beta feature*
+
+❒『 *MAKER* 』
+│
+┝⋗ *${prefix}sand1
+┝⋗ *${prefix}sand2
+┝⋗ *${prefix}tahta*
+┝⋗ *${prefix}bplogo*
+┝⋗ *${prefix}phlogo*
+┝⋗ *${prefix}ice*
+┝⋗ *${prefix}3dbox*
+┝⋗ *${prefix}road*
+┝⋗ *${prefix}battlefield4*
+┝⋗ *${prefix}halloween*
+┝⋗ *${prefix}code*
+┝⋗ *${prefix}gelas1*
+┝⋗ *${prefix}gelas2*
+┝⋗ *${prefix}breakwall*
+┝⋗ *${prefix}bneon*
+┝⋗ *${prefix}gneon*
+┝⋗ *${prefix}dropwater*
+┝⋗ *${prefix}narto*
+┝⋗ *${prefix}burnpaper*
+┝⋗ *${prefix}shadow*
+┝⋗ *${prefix}glitch*
+┝⋗ *${prefix}candy*
+┝⋗ *${prefix}coffe*
+┝⋗ *${prefix}coffee*
+┝⋗ *${prefix}8bit*
+┕━━━━❪¢❫━━━━
+
+
+❒─⟮ *𝗙𝗨𝗡&𝗚𝗔𝗠𝗘𝗦* ⟯
+│
+┝⋗ *${prefix}truth*
+┝⋗ *${prefix}jadian*
+┝⋗ *${prefix}tebakgambar*
+┝⋗ *${prefix}dare*
+┝⋗ *${prefix}caklontong*
+┝⋗ *${prefix}cekjodoh*
+┝⋗ *${prefix}apakah*
+┝⋗ *${prefix}kapankah*
+┝⋗ *${prefix}bisakah*
+┝⋗ *${prefix}rate*
+┝⋗ *${prefix}watak*
+└⋗ *${prefix}hobby*
+
+
+❒─⟮ *𝗠𝗘𝗗𝗜𝗔* ⟯
+│
+┝⋗ *${prefix}stiker*
+┝⋗ *${prefix}bass*
+┝⋗ *${prefix}nightcore*
+┝⋗ *${prefix}playstore*
+┝⋗ *${prefix}googleimg*
+┝⋗ *${prefix}apksearch*
+┝⋗ *${prefix}toimg*
+┝⋗ *${prefix}nulis*
+┝⋗ *${prefix}ocr*
+┝⋗ *${prefix}ytsearch* 
+┝⋗ *${prefix}ytmp3 [ERROR]*
+┝⋗ *${prefix}tiktok*
+┝⋗ *${prefix}tiktokstalk*
+┝⋗ *${prefix}igstalk* [ERROR]
+┝⋗ *${prefix}pinterest*
+┝⋗ *${prefix}tts*
+┝⋗ *${prefix}tes*
+┝⋗ *${prefix}r-alquran*
+┝⋗ *${prefix}tep*
+┝⋗ *${prefix}ssweb*
+┝⋗ *${prefix}url2image*
+┝⋗ *${prefix}img2url*
+┝⋗ *${prefix}kbbi*
+┝⋗ *${prefix}kpop*
+└⋗ *${prefix}wait*
+
+
+❒─⟮ *🄰🄽🄸🄼🄴* ⟯
+│
+┝⋗ *${prefix}elf*
+┝⋗ *${prefix}chara-search*
+┝⋗ *${prefix}samehadaku*
+┝⋗ *${prefix}dewabatch* ᵉʳʳᵒʳ
+┝⋗ *${prefix}erofeet*
+┝⋗ *${prefix}nekoyuri*
+┝⋗ *${prefix}ahegao*
+┝⋗ *${prefix}anime* ᴮᵁᴳ
+┝⋗ *${prefix}nekonime*
+┝⋗ *${prefix}loli* 
+┝⋗ *${prefix}loli2* ᴮᵁᴳ
+┝⋗ *${prefix}waifu*
+┝⋗ *${prefix}waifu2*
+┝⋗ *${prefix}wibu*
+┝⋗ *${prefix}randomanime* 
+┝⋗ *${prefix}shota* 
+┝⋗ *${prefix}foxgirl*
+┝⋗ *${prefix}neonime*
+└⋗ *${prefix}husbu*
+
+❒─⟮ *I͟N͟F͟O͟&E͟D͟U͟K͟A͟S͟I͟* ⟯
+│
+┝⋗ *${prefix}infogc*
+┝⋗ *${prefix}infogempa*
+┝⋗ *${prefix}infocuaca*
+┝⋗ *${prefix}grupinfo*
+┝⋗ *${prefix}lirik*
+┝⋗ *${prefix}quotes*
+┝⋗ *${prefix}chord*
+┝⋗ *${prefix}wiki*
+┝⋗ *${prefix}brainly*
+└⋗ *${prefix}resepmasakan*
+
+❒─⟮ *GRUP* ⟯
+│
+┝⋗ *${prefix}add*
+┝⋗ *${prefix}kick*
+┝⋗ *${prefix}promote*
+┝⋗ *${prefix}demote*
+┝⋗ *${prefix}setname*
+┝⋗ *${prefix}setdesc*
+┝⋗ *${prefix}welcome*
+┝⋗ *${prefix}nsfw*
+┝⋗ *${prefix}simih*
+┝⋗ *${prefix}grup [buka/tutup]*
+┝⋗ *${prefix}tagme*
+┝⋗ *${prefix}hidetag*
+┝⋗ *${prefix}tagall*
+┝⋗ *${prefix}otagall*
+┝⋗ *${prefix}fitnah*
+┝⋗ *${prefix}infogc*
+┝⋗ *${prefix}grupinfo*
+┝⋗ *${prefix}linkgrup*
+┝⋗ *${prefix}listadmins*
+└⋗ *${prefix}openanime*
+
+
+❒─⟮ *N̥S̥F̥W̥* ⟯ 
+│
+┝⋗ *${prefix}nsfw* 1/0 
+┝⋗ *${prefix}ecchi*
+┝⋗ *${prefix}nsfw-waifu*
+┝⋗ *${prefix}yuri*
+┝⋗ *${prefix}futanari*
+┝⋗ *${prefix}cuddle*
+┝⋗ *${prefix}smallboobs*
+┝⋗ *${prefix}ero*
+┝⋗ *${prefix}femdom*
+┝⋗ *${prefix}kuni*
+┝⋗ *${prefix}les*
+┝⋗ *${prefix}eroyuri*
+┝⋗ *${prefix}holo*
+┝⋗ *${prefix}xvideo*
+┝⋗ *${prefix}hololewd*
+┝⋗ *${prefix}yuri*
+┕━━━━━━━━━━━━
+
+❒─⟮ *𝗢𝗪𝗡𝗘𝗥* ⟯
+│
+┝⋗ *${prefix}bc*
+┝⋗ *${prefix}block*
+┝⋗ *${prefix}unblock*
+┝⋗ *${prefix}clearall*
+┝⋗ *${prefix}clone*
+┝⋗ *${prefix}getses*
+┝⋗ *${prefix}setpp*
+┝⋗ *${prefix}setpp*
+┝⋗ *${prefix}leave*
+┝⋗ *${prefix}fake*
+┝⋗ *${prefix}fitnah*
+│
+┢━━━━━❪❐❫━━━━━
+┣━━‣ *𝙏𝙝𝙖𝙣𝙠*
+┃
+┃◎𝙇-𝙈𝙊 
+┃◎𝙍𝘼𝘿𝙔𝘼
+┃◎𝙃𝙆 𝟰𝟭𝟲
+┃
+┃◎𝗠𝗵𝗮𝗻𝗸𝗕𝗮𝗿𝗕𝗮𝗿
+┃
+┗━━━━━❪❐❫━━━━━
+`
+}
+exports.help = help
